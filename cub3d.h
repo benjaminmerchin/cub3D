51b1eccd8987b5b0cdca09add0e7139c4b41a9ea
benjaminmerchin <benjaminmerchin@gmail.com>
2021-01-08 13:54:35 +0100

    oui

---
+++ cub3d.h
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmerchin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/04 02:02:12 by bmerchin          #+#    #+#             */
+/*   Updated: 2021/01/08 13:53:16 by bmerchin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "mlx.h"
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+
+#define BUFFER_SIZE 4095
+
+typedef struct	s_struct
+{
+	char	buff[BUFFER_SIZE + 1];
+	int		ret;
+	int		curs;
+}				t_struct;
+
+typedef struct s_data
+{
+	char			no[4096];
+	char			so[4096];
+	char			we[4096];
+	char			ea[4096];
+	char			sprite[4096];
+	unsigned short	x_screen_size;
+	unsigned short	y_screen_size;
+	int				floor;
+	int				ceiling;
+	char			map[512][512];
+	char			direction_player_start;
+	short			x_player_start;
+	short			y_player_start;
+}				t_data;
+
+int				get_next_line(int fd, char **line);
+int				get_next_line2(int fd, char **line, t_struct *dta);
+int				ft_strlen(char *str);
+int				ft_strlenn(char *str);
+char			*ft_strjoin(char *s1, char *s2, t_struct *dta);
+char			*free_null(char *s1);
+int				free_int(char **line);
+
+void			ft_putstr_bn(char *str);
+
+#endif
--- training_mlx_functions.c
+++ training_mlx_functions.c
@@ -1,18 +1,69 @@
-#include "mlx.h"
-#include <unistd.h>
-
-int main(void)
+#include "cub3d.h"
+/*
+void check_map(t_struct *data)
 {
-	void *mlx;
-	void *window;
+	
+	
+}*/
+
+//void check_missing
+
+//void store_data
+
+/*int main(void)
+{
+//	void *mlx;
+//	void *window;
+//	t_data	data;
   
 	write(1, "124", 3);
-  	mlx = mlx_init();
+//  	mlx = mlx_init();
 //	(void)mlx;
-	window = mlx_new_window(mlx, 400, 400, "Title");
-	mlx_loop(mlx);
-	write(1, "1", 1);
+//	window = mlx_new_window(mlx, 400, 400, "Title");
+//	mlx_loop(mlx);
+	
+	
+
+	ft_putstr_bn("ok");
 
 //	free(mlx);
   	return (0);
+}*/
+
+int main(int ac, char **av)
+{
+	int fd;
+	int retour;
+	char *line;
+	t_data data;
+
+	(void)data;
+	line = NULL;
+	if (ac != 2)
+	{
+		ft_putstr_bn("Wrong number of arguments");
+		return (0);
+	}
+	if ((fd = open(av[1], O_RDONLY)) < 0)
+	{
+		ft_putstr_bn("OpenError");
+		return (0);
+	}
+	while ((retour = get_next_line(fd, &line)) == 1)
+	{
+		ft_putstr_bn(line);
+		free(line);
+	}
+
+/*	if (line[0] != 0)
+	{
+		ft_putstr_bn(line);
+		free(line);
+	}*/
+/*	ft_putstr_bn("");
+	ft_putstr_bn("segfaul error[1]");
+	while (fd < 1500000000)
+		fd++;
+	ft_putstr_bn("nan j'dec");*/
+	return (0);
 }
